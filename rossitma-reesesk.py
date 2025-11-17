b0c27b52b69ab0d1c0f0e243285748a374dbb811
Sammi Reese <reesesk@dukes.jmu.edu>
2025-11-17 12:52:37 -0500

    Update rossitma-reesesk.py

--- rossitma-reesesk.py
+++ rossitma-reesesk.py
@@ -19,12 +19,12 @@ import sys
 
 def load_and_display_file():
     graph1 = nx.read_graphml('small-bitcoingraph.graphml')
-    return graph1 
-    
+    return graph1
+
 
 def main():
     graph1 = load_and_display_file()
-    
+
     # question 1 total nodes
     all_nodes = graph1.nodes()
     print("How many bitcoin nodes does the bitcoin network in the data file have?")
@@ -41,7 +41,7 @@ def main():
     for node in all_nodes:
         if graph1.degree(node) > max_degree:
             max_degree = graph1.degree(node)
-    
+
     nodes_largest_degree = []
     for node in graph1.nodes():
         if graph1.degree(node) == max_degree:
@@ -56,7 +56,7 @@ def main():
     for node in all_nodes:
         if graph1.degree(node) < min_degree:
             min_degree = graph1.degree(node)
-    
+
     nodes_smallest_degree = []
     for node in graph1.nodes():
         if graph1.degree(node) == min_degree:
@@ -66,8 +66,26 @@ def main():
     print("The following nodes have the smallest degree: " + str(nodes_smallest_degree) + " with a degree of " + str(min_degree))
 
 
+    #Question 5 Top 10 nodes by degree
+    degrees = dict(graph1.degree())
+    top_10_nodes = sorted(degrees.items(), key=lambda x: x[1], reverse=True)[:10]
+
+    print("What are the top 10 nodes by degree?")
+    for node, deg in top_10_nodes:
+        ip = graph1.nodes[node].get('ip', 'N/A')
+        country = graph1.nodes[node].get('country', 'N/A')
+        print(f"IP: {ip}, Degree: {deg}, Country: {country}")
 
 
+    #Question 6 top 5 countries by # of nodes
+    from collections import Counter
+    countries = [graph1.nodes[n].get('country', 'N/A') for n in graph1.nodes()]
+    country_counts = Counter(countries)
+    top_5_countries = country_counts.most_common(5)
+
+    print("What are the top 5 countries by number of nodes?")
+    for country, count in top_5_countries:
+        print(f"{country}: {count}")
 
 if __name__ == "__main__":
     main()
